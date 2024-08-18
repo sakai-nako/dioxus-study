27d527ec9e68f1c86eb2f0fa72e0f74956ad9f9c
sakai-nako <80052740+sakai-nako@users.noreply.github.com>
2024-08-18 21:13:11 +0900

    refactor: Clean up main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,6 @@
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
 
-#[derive(Clone, Routable, Debug, PartialEq)]
-enum Route {
-    #[route("/")]
-    Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
-}
-
 fn main() {
     // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
@@ -24,29 +16,15 @@ fn App() -> Element {
     }
 }
 
-#[component]
-fn Blog(id: i32) -> Element {
-    rsx! {
-        Link { to: Route::Home {}, "Go to counter" }
-        "Blog post {id}"
-    }
+#[derive(Clone, Routable, Debug, PartialEq)]
+enum Route {
+    #[route("/")]
+    Home {},
 }
 
 #[component]
 fn Home() -> Element {
-    let mut count = use_signal(|| 0);
-
     rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog"
-        }
-        div {
-            h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
-        }
+        h2 { "Hello, World!" }
     }
 }
